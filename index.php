9da102613b4c204a687b9fe29ca220ae116c73f3
sonico893 <byronzeas@gmail.com>
2024-11-24 19:36:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio del front end</title>
+    <title>Inicio del front end 2</title>
 </head>
 <body>
 <h1>Hola </h1>
