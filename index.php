28ee6a25b7a79cdbc788dee3bf896ca07cd9b0b3
Sonico893 <byronzeas@gmail.com>
2024-11-24 19:22:35 -0500

    prueba php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio del front end</title>
+</head>
+<body>
+<h1>Hola </h1>
+
+<?php
+    
+    echo "<h1>Hola </h1>";
+?>
+
+    
+</body>
+</html>
